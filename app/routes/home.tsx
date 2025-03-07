5e8242b5d715ccf88379bc5992428bfbf7695688
smithwithatypo <smithwithatypo@gmail.com>
2025-03-07 14:43:54 -0600

    add shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
+// This meta export provides metadata for this route
+// Similar to Angular's route data, but more powerful
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -8,6 +10,8 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+// This default export is the component that will render when this route is active
+// No need to register this route elsewhere - the file path itself serves as the route registration
 export default function Home() {
   return <Welcome />;
 }
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,9 +1,18 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import { Button } from "app/components/ui/button"
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
+
+    {/* new button here */}
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>new button</Button>
+      <Button variant="secondary">Secondary button</Button>
+    </div>
+
+
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
