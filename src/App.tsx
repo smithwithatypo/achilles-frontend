a2f01a27d6e61ed377f63f9c3cfe8724329d9719
smithwithatypo <smithwithatypo@gmail.com>
2025-03-08 23:12:30 -0600

    add dark mode but needs debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { ThemeProvider } from "@/components/ui/theme-provider"
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -9,14 +10,16 @@ import './App.css'
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+        <Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+    </ThemeProvider>
+        </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@
 import { Outlet, Link } from 'react-router';
 import { Button } from "@/components/ui/button";
 import { navItems } from '@/routes';
+import { ModeToggle } from '@/components/ui/mode-toggle'
 
 const Layout: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const Layout: React.FC = () => {
               <Link to={item.path}>{item.label}</Link>
             </Button>
           ))}
+          <ModeToggle />
         </nav>
       </header>
       
@@ -21,7 +23,7 @@ const Layout: React.FC = () => {
       </main>
       
       <footer>
-        <p>© Your Education App</p>
+        <p>© Stephen Smyth</p>
       </footer>
     </div>
   );
