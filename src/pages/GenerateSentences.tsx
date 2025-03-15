2b46120517d6fb7eb279419f38ec53902b367c47
smithwithatypo <smithwithatypo@gmail.com>
2025-03-15 16:05:33 -0500

    testing env variables in produ

--- src/pages/GenerateSentences.tsx
+++ src/pages/GenerateSentences.tsx
@@ -49,13 +49,20 @@ const GenerateSentences: React.FC = () => {
     const handleButtonClick = async () => {
         setIsLoading(true);
         setError(null);
-        const apiUrl = import.meta.env.VITE_API_URL;
-        console.log("api url outside try catch:", apiUrl);
         
         try {
+            const apiUrl = "http://localhost:8080"
             // const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
-            const apiUrl = import.meta.env.VITE_API_URL;
-            console.log("api url inside try catch:", apiUrl);
+            if (import.meta.env.PROD) {
+                const apiUrl = "https://backend-production-cb2a.up.railway.app"
+                console.log("api url inside prod:", apiUrl);
+            } else {
+                const apiUrl = "http://localhost:8080"
+                console.log("api url inside dev:", apiUrl);
+            }
+            // const apiUrl = "https://backend-production-cb2a.up.railway.app"
+            // console.log("api url inside try catch:", apiUrl);
+            
             const response = await axios.post(`${apiUrl}/sentences`, {
                 "prompt": prompt,
                 "model": modelChoice
