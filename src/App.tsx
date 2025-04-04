c5bc9019dbf4242d258d035515d87c019e9383f5
smithwithatypo <smithwithatypo@gmail.com>
2025-04-04 17:26:52 -0500

    delete dashboard from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
-import Dashboard from './pages/Dashboard';
 import GenerateSentences from './pages/GenerateSentences'
 import NotFound from './pages/NotFound';
 import './App.css'
@@ -15,7 +14,6 @@ const App: React.FC = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
-            <Route path="dashboard" element={<Dashboard />} />
             <Route path="generateSentences" element={<GenerateSentences />} />
             <Route path="*" element={<NotFound />} />
           </Route>
